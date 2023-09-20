66b2c75b67a0b6c5503b5cf37a536b0b5b0db438
Mads Mikkel Rasmussen <mara@aspit.dk>
2023-09-20 11:17:14 +0200

    Add project files.

---
+++ Ch04/Ex_0412/Program_0412.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex_0412
+{
+    internal class Program_0412
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
